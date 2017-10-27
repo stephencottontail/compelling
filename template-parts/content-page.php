b818c09ebdc02a4ef7482ba8735803d2c89a28ce
Stephen Dickinson <stephencottontail@me.com>
2017-10-27 15:30:39 -0600

    Had to correct indentation

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( has_post_thumbnail() ) : ?>
 		<figure class="entry-thumbnail">
-		  <?php the_post_thumbnail(); ?>
+			<?php the_post_thumbnail(); ?>
 		</figure>
 	<?php endif; ?>	
 
