3cd01fdffc76e9273ced6bb123d04d81c4436a32
Stephen Dickinson <stephencottontail@me.com>
2017-10-30 09:39:03 -0600

    Fixes #10

--- comments.php
+++ comments.php
@@ -26,23 +26,7 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
-			<?php
-				$comment_count = get_comments_number();
-				if ( 1 === $comment_count ) {
-					printf(
-						/* translators: 1: title. */
-						_e( 'One thought on &ldquo;%1$s&rdquo;', 'compelling' ),
-						'<span>' . get_the_title() . '</span>'
-					);
-				} else {
-					printf( // WPCS: XSS OK.
-						/* translators: 1: comment count number, 2: title. */
-						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'compelling' ) ),
-						number_format_i18n( $comment_count ),
-						'<span>' . get_the_title() . '</span>'
-					);
-				}
-			?>
+			<?php printf( __( 'Comments (%s)', 'compelling' ), number_format_i18n( get_comments_number() ) ); ?>
 		</h2><!-- .comments-title -->
 
 		<ol class="comment-list">
